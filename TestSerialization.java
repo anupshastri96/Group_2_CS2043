f48d71ab2a1ba711a57507e4b176e98e20d43d5e
MaxMacNeill-UNB <d3n2c@unb.ca>
2023-10-31 14:37:16 -0300

    Update TestSerialization.java
    Implemented the testing logic

--- TestSerialization.java
+++ TestSerialization.java
@@ -1,8 +1,11 @@
 import BackEnd.Ingredients.*;
 import java.util.ArrayList;
 import java.io.File;
+import java.io.IOException;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectOutputStream;
 
 public class TestSerialization {
     private static String userHas(Ingredient in) {
@@ -13,12 +16,16 @@ public class TestSerialization {
 
     public static void main(String[] args) {
         String importPath;
+        boolean imported;
         File f = new File("./savedIngredients.bin");
         if(f.exists() && !f.isDirectory()) { 
+            System.out.println("Saved Ingredients Array found, using it"); 
             importPath = "./savedIngredients.bin";
+            imported = true;
         } else {
             System.out.println("Saved Ingredients Array not found, using default"); 
             importPath = "./BackEnd/Ingredients/defaultIngredients.bin";
+            imported = false;
         }
 
         ArrayList<Ingredient> ingredientsList = new ArrayList<Ingredient>(); //This will be discarded when it is set by the below code, but I needed it to get rid of some compiler uninitialized variable warnings.
@@ -38,5 +45,30 @@ public class TestSerialization {
         for (int i = 0; i < ingredientsList.size(); i++) {
             System.out.println("Loaded Ingredient: " + ingredientsList.get(i).getName() + ", " + userHas(ingredientsList.get(i)));
         }
+
+        /* 
+         * Actual Logic Goes here. time to do some funky stuff for testing purposes.
+        */
+        if (!imported) {
+
+            Ingredient thyme = new Ingredient("Thyme");
+            ingredientsList.add(thyme);
+
+            Ingredient carrots = ingredientsList.get(0);
+            Ingredient potatoes = ingredientsList.get(2);
+            Ingredient corn = ingredientsList.get(3);
+
+            thyme.setAvailable(true);
+            carrots.setAvailable(true);
+            potatoes.setAvailable(true);
+            corn.setAvailable(true);
+
+            try(ObjectOutputStream objOut = new ObjectOutputStream(new FileOutputStream("./savedIngredients.bin"))) {
+                objOut.writeObject(ingredientsList);
+            }
+            catch (IOException ioe) {
+                System.out.println("IO Exception Thrown: " + ioe.getMessage());
+            }
+        } else f.delete();
     }
 }
