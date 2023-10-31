41968bb9079aacccb5965f291fb37c0930058e9e
MaxMacNeill-UNB <d3n2c@unb.ca>
2023-10-31 13:40:23 -0300

    Update Ingredient.java

--- BackEnd/Ingredients/Ingredient.java
+++ BackEnd/Ingredients/Ingredient.java
@@ -1,5 +1,19 @@
 package BackEnd.Ingredients;
 
 public class Ingredient {
+    private String name;
+    private boolean available;
 
+    public Ingredient(String nameIn) {
+        name = nameIn;
+        available = false;
+    }
+
+    public void setAvailable(boolean in) {
+        available = in;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
 }
